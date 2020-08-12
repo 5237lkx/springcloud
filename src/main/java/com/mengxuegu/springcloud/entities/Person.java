5d2ad64eca74b0007717cd287db97659b14ee7bf
lvkangxin <894296604@qq.com>
2020-08-12 14:59:13 +0800

    test commit

--- src/main/java/com/mengxuegu/springcloud/entities/Person.java
+++ src/main/java/com/mengxuegu/springcloud/entities/Person.java
@@ -6,6 +6,10 @@ public class Person {
 
     private Integer age;
 
+    private String testBranch;
+
+    private String lkx1;
+
     public String getName() {
         return name;
     }
@@ -20,5 +24,33 @@ public class Person {
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public String getTestBranch() {
+        return testBranch;
+    }
+
+    public void setTestBranch(String testBranch) {
+        this.testBranch = testBranch;
+    }
+
+    //
+
+
+    public String getLkx1() {
+        return lkx1;
+    }
+
+    public void setLkx1(String lkx1) {
+        this.lkx1 = lkx1;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", testBranch='" + testBranch + '\'' +
+                '}';
     }
 }
